a83a7e78013c9bf2657e343bd71d92f52b5a0fbe
jithset <jithset@gmail.com>
2022-02-15 16:28:18 +0530

    aws s3

---
+++ aws-s3-demo/src/test/java/io/github/jithset/awss3demo/AwsS3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jithset.awss3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

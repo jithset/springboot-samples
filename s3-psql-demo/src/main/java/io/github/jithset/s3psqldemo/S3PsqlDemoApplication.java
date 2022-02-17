a364435f0e41f1330eeccf90f02cd077bb9ab87a
jithset <jithset@gmail.com>
2022-02-17 16:00:38 +0530

    psql s3

---
+++ s3-psql-demo/src/main/java/io/github/jithset/s3psqldemo/S3PsqlDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.jithset.s3psqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3PsqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3PsqlDemoApplication.class, args);
+	}
+
+}
---
+++ s3-psql-demo/src/test/java/io/github/jithset/s3psqldemo/S3PsqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jithset.s3psqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3PsqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
